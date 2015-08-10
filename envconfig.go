ec080dd39bcb2273ef91c2e9d2a463b96f8c0e97
Matt Aitchison <Matt@lanciv.com>
2015-08-10 14:41:57 -0500

    Removed code generation, added EnvSet, moved around some code.

--- envconfig.go
+++ envconfig.go
@@ -1,14 +1,20 @@
-//go:generate go run gen/generate.go
-
 package envconfig
 
 import (
 	"fmt"
+	"io"
+	"net"
 	"os"
 	"strings"
+	"time"
 )
 
-var environment = make(map[string]*ConfigVar)
+type EnvSet struct {
+	name   string
+	actual map[string]*ConfigVar
+	formal map[string]*ConfigVar
+	output io.Writer // nil means stderr; use out() accessor
+}
 
 // ConfigVar represents a value from the environment.
 type ConfigVar struct {
@@ -18,46 +24,305 @@ type ConfigVar struct {
 	Default     string // default value (as text); for description message
 }
 
+func (e *EnvSet) out() io.Writer {
+	if e.output == nil {
+		return os.Stderr
+	}
+	return e.output
+}
+
+// SetOutput sets the destination for usage and error messages.
+// If output is nil, os.Stderr is used.
+func (e *EnvSet) SetOutput(output io.Writer) {
+	e.output = output
+}
+
+// String retrieves a environment variable by name and parses it to a string
+// defaultVal will be returned if the variable is not found.
+func (e *EnvSet) String(name string, defaultVal string, description string) string {
+	v := e.NewVar(newStringValue(defaultVal), name, description)
+	return v.Value.Get().(string)
+}
+
+// String retrieves a environment variable by name and parses it to a string
+// defaultVal will be returned if the variable is not found.
+func String(name string, defaultVal string, description string) string {
+	return DefaultEnv.String(name, defaultVal, description)
+}
+
+// StringOption like String except the value must be in options.
+// defaultVal will be returned if the variable is not found or is not a valid option.
+func (e *EnvSet) StringOption(name string, defaultVal string, options []string, description string) string {
+	v := e.NewVar(newStringValue(defaultVal), name, description)
+	for _, option := range options {
+		if option == v.Value.Get().(string) {
+			return v.Value.Get().(string)
+		}
+	}
+	return defaultVal
+}
+
+// StringOption like String except the value must be in options.
+// defaultVal will be returned if the variable is not found or is not a valid option.
+func StringOption(name string, defaultVal string, options []string, description string) string {
+	return DefaultEnv.StringOption(name, defaultVal, options, description)
+}
+
+// Bool retrieves a environment variable by name and parses it to a bool
+// defaultVal will be returned if the variable is not found.
+func (e *EnvSet) Bool(name string, defaultVal bool, description string) bool {
+	v := e.NewVar(newBoolValue(defaultVal), name, description)
+	return v.Value.Get().(bool)
+}
+
+// Bool retrieves a environment variable by name and parses it to a bool
+// defaultVal will be returned if the variable is not found.
+func Bool(name string, defaultVal bool, description string) bool {
+	return DefaultEnv.Bool(name, defaultVal, description)
+}
+
+// Float64 retrieves a environment variable by name and parses it to a float64
+// defaultVal will be returned if the variable is not found.
+func (e *EnvSet) Float64(name string, defaultVal float64, description string) float64 {
+	v := e.NewVar(newFloat64Value(defaultVal), name, description)
+	return v.Value.Get().(float64)
+}
+
+// Float64 retrieves a environment variable by name and parses it to a float64
+// defaultVal will be returned if the variable is not found.
+func Float64(name string, defaultVal float64, description string) float64 {
+	return DefaultEnv.Float64(name, defaultVal, description)
+}
+
+// Float64Option like Float64 except the value must be in options.
+// defaultVal will be returned if the variable is not found or is not a valid option.
+func (e *EnvSet) Float64Option(name string, defaultVal float64, options []float64, description string) float64 {
+	v := e.NewVar(newFloat64Value(defaultVal), name, description)
+	for _, option := range options {
+		if option == v.Value.Get().(float64) {
+			return v.Value.Get().(float64)
+		}
+	}
+	return defaultVal
+}
+
+// Int retrieves a environment variable by name and parses it to a int
+// defaultVal will be returned if the variable is not found.
+func (e *EnvSet) Int(name string, defaultVal int, description string) int {
+	v := e.NewVar(newIntValue(defaultVal), name, description)
+	return v.Value.Get().(int)
+}
+
+// Int retrieves a environment variable by name and parses it to a int
+// defaultVal will be returned if the variable is not found.
+func Int(name string, defaultVal int, description string) int {
+	return DefaultEnv.Int(name, defaultVal, description)
+}
+
+// IntOption like Int except the value must be in options.
+// defaultVal will be returned if the variable is not found or is not a valid option.
+func (e *EnvSet) IntOption(name string, defaultVal int, options []int, description string) int {
+	v := e.NewVar(newIntValue(defaultVal), name, description)
+	for _, option := range options {
+		if option == v.Value.Get().(int) {
+			return v.Value.Get().(int)
+		}
+	}
+	return defaultVal
+}
+
+// Int64 retrieves a environment variable by name and parses it to a int64
+// defaultVal will be returned if the variable is not found.
+func (e *EnvSet) Int64(name string, defaultVal int64, description string) int64 {
+	v := e.NewVar(newInt64Value(defaultVal), name, description)
+	return v.Value.Get().(int64)
+}
+
+// Int64 retrieves a environment variable by name and parses it to a int64
+// defaultVal will be returned if the variable is not found.
+func Int64(name string, defaultVal int64, description string) int64 {
+	return DefaultEnv.Int64(name, defaultVal, description)
+}
+
+// Int64Option like Int64 except the value must be in options.
+// defaultVal will be returned if the variable is not found or is not a valid option.
+func (e *EnvSet) Int64Option(name string, defaultVal int64, options []int64, description string) int64 {
+	v := e.NewVar(newInt64Value(defaultVal), name, description)
+	for _, option := range options {
+		if option == v.Value.Get().(int64) {
+			return v.Value.Get().(int64)
+		}
+	}
+	return defaultVal
+}
+
+// Uint retrieves a environment variable by name and parses it to a uint
+// defaultVal will be returned if the variable is not found.
+func (e *EnvSet) Uint(name string, defaultVal uint, description string) uint {
+	v := e.NewVar(newUintValue(defaultVal), name, description)
+	return v.Value.Get().(uint)
+}
+
+// Uint retrieves a environment variable by name and parses it to a uint
+// defaultVal will be returned if the variable is not found.
+func Uint(name string, defaultVal uint, description string) uint {
+	return DefaultEnv.Uint(name, defaultVal, description)
+}
+
+// UintOption like Uint except the value must be in options.
+// defaultVal will be returned if the variable is not found or is not a valid option.
+func (e *EnvSet) UintOption(name string, defaultVal uint, options []uint, description string) uint {
+	v := e.NewVar(newUintValue(defaultVal), name, description)
+	for _, option := range options {
+		if option == v.Value.Get().(uint) {
+			return v.Value.Get().(uint)
+		}
+	}
+	return defaultVal
+}
+
+// Uint64 retrieves a environment variable by name and parses it to a uint64
+// defaultVal will be returned if the variable is not found.
+func (e *EnvSet) Uint64(name string, defaultVal uint64, description string) uint64 {
+	v := e.NewVar(newUint64Value(defaultVal), name, description)
+	return v.Value.Get().(uint64)
+}
+
+// Uint64 retrieves a environment variable by name and parses it to a uint64
+// defaultVal will be returned if the variable is not found.
+func Uint64(name string, defaultVal uint64, description string) uint64 {
+	return DefaultEnv.Uint64(name, defaultVal, description)
+}
+
+// Uint64Option like Uint64 except the value must be in options.
+// defaultVal will be returned if the variable is not found or is not a valid option.
+func (e *EnvSet) Uint64Option(name string, defaultVal uint64, options []uint64, description string) uint64 {
+	v := e.NewVar(newUint64Value(defaultVal), name, description)
+	for _, option := range options {
+		if option == v.Value.Get().(uint64) {
+			return v.Value.Get().(uint64)
+		}
+	}
+	return defaultVal
+}
+
+func (e *EnvSet) Duration(name string, defaultVal time.Duration, description string) time.Duration {
+	v := e.NewVar(newDurationValue(defaultVal), name, description)
+	return v.Value.Get().(time.Duration)
+}
+
+func Duration(name string, defaultVal time.Duration, description string) time.Duration {
+	return DefaultEnv.Duration(name, defaultVal, description)
+}
+
+// IP retrieves a environment variable by name and parses it to a net.IP
+// defaultVal will be returned if the variable is not found.
+func (e *EnvSet) IP(name string, defaultVal net.IP, description string) net.IP {
+	v := e.NewVar(newIPValue(defaultVal), name, description)
+	return v.Value.Get().(net.IP)
+}
+
+// IP retrieves a environment variable by name and parses it to a net.IP
+// defaultVal will be returned if the variable is not found.
+func IP(name string, defaultVal net.IP, description string) net.IP {
+	return DefaultEnv.IP(name, defaultVal, description)
+}
+
+func (e *EnvSet) VisitAll(fn func(*ConfigVar)) {
+	for _, cfg := range e.actual {
+		fn(cfg)
+	}
+}
+
+func VisitAll(fn func(*ConfigVar)) {
+	DefaultEnv.VisitAll(fn)
+}
+
 // NewVar retrieves a variable from the environment that is of type Value.
-func NewVar(value Value, name string, description string) *ConfigVar {
+func (e *EnvSet) NewVar(value Value, name string, description string) *ConfigVar {
 	envVar := &ConfigVar{
 		Name:        strings.ToUpper(name),
 		Description: description,
 		Value:       value,
 		Default:     value.String(),
 	}
-	_, defined := environment[name]
+	_, defined := e.actual[name]
 	if defined {
 		panic("env: " + name + " already defined.")
 	}
 
-	actual := os.Getenv(envVar.Name)
-	if actual != "" {
-		envVar.Value.Set(actual)
+	// This step is part of Parse() in flags pkg.
+	if v := os.Getenv(envVar.Name); v != "" {
+		envVar.Value.Set(v)
 	}
 
-	environment[name] = envVar
+	if e.actual == nil {
+		e.actual = make(map[string]*ConfigVar)
+	}
+	e.actual[name] = envVar
 
 	return envVar
 }
 
+// NewVar retrieves a variable from the environment that is of type Value.
+func NewVar(value Value, name string, description string) *ConfigVar {
+	return DefaultEnv.NewVar(value, name, description)
+}
+
 // Var retrieves a ConfigVar by name from the ConfigVar map.
-func Var(name string) *ConfigVar {
-	if v, ok := environment[name]; ok {
+func (e *EnvSet) Var(name string) *ConfigVar {
+	if v, ok := e.actual[name]; ok {
 		return v
 	}
 	return nil
 }
 
+// Var retrieves a ConfigVar by name from the ConfigVar map.
+func Var(name string) *ConfigVar {
+	return DefaultEnv.Var(name)
+}
+
+// Vars retrieve all ConfigVars from the ConfigVar map.
+func (e *EnvSet) Vars() map[string]*ConfigVar {
+	return e.actual
+}
+
 // Vars retrieve all ConfigVars from the ConfigVar map.
 func Vars() map[string]*ConfigVar {
-	return environment
+	return DefaultEnv.Vars()
+}
+
+// PrintDefaults prints, to stderr, the default values of all defined ConfigVars.
+func (e *EnvSet) PrintDefaults() {
+	for _, v := range e.actual {
+		env := fmt.Sprintf("%s=%q", v.Name, v.Default)
+		fmt.Fprintf(os.Stderr, "%-30s # %s\n", env, v.Description)
+	}
 }
 
 // PrintDefaults prints, to stderr, the default values of all defined ConfigVars.
 func PrintDefaults() {
-	for _, v := range environment {
-		env := fmt.Sprintf("%s=%q", v.Name, v.Default)
-		fmt.Fprintf(os.Stderr, "%-30s # %s\n", env, v.Description)
+	DefaultEnv.PrintDefaults()
+}
+
+// PrintEnv prints, to stderr, the set values of all defined ConfigVars.
+func (e *EnvSet) PrintEnv() {
+	for _, v := range e.actual {
+		env := fmt.Sprintf("%s=%q", v.Name, v.Value)
+		fmt.Fprintf(e.out(), "%-30s # %s\n", env, v.Description)
 	}
+}
+
+// PrintEnv prints, to stderr, the set values of all defined ConfigVars.
+func PrintEnv() {
+	DefaultEnv.PrintEnv()
+}
+
+var DefaultEnv = NewEnvSet(os.Args[0])
+
+func NewEnvSet(name string) *EnvSet {
+	e := &EnvSet{
+		name: name,
+	}
+	return e
 }
--- envconfig_test.go
+++ envconfig_test.go
@@ -1,28 +1,105 @@
 package envconfig
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
+func boolString(s string) string {
+	if s == "0" {
+		return "false"
+	}
+	return "true"
+}
+
+func ResetForTesting() {
+	DefaultEnv = NewEnvSet(os.Args[0])
+}
+
+func TestEverything(t *testing.T) {
+	ResetForTesting()
+	Bool("test_bool", false, "bool value")
+	Int("test_int", 0, "int value")
+	Int64("test_int64", 0, "int64 value")
+	Uint("test_uint", 0, "uint value")
+	Uint64("test_uint64", 0, "uint64 value")
+	String("test_string", "0", "string value")
+	StringOption("test_string_opt", "0", []string{"0", "2"}, "string opt")
+	Float64("test_float64", 0, "float64 value")
+	Duration("test_duration", 0, "time.Duration value")
+
+	desired := "0"
+	visitor := func(c *ConfigVar) {
+		if len(c.Name) > 5 && c.Name[0:5] == "TEST_" {
+			ok := false
+			switch {
+			case c.Value.String() == desired:
+				ok = true
+			case c.Name == "TEST_BOOL" && c.Value.String() == boolString(desired):
+				ok = true
+			case c.Name == "TEST_DURATION" && c.Value.String() == desired+"s":
+				ok = true
+			}
+			if !ok {
+				t.Error(c.Value.String(), c.Name)
+			}
+		}
+	}
+	VisitAll(visitor)
+}
+
+func TestEnvSet(t *testing.T) {
+	ResetForTesting()
+	String("test_string", "0", "string value")
+	set := NewEnvSet("test")
+
+	str := set.String("test_string_set", "hello", "string value")
+	if str != "hello" {
+		t.Error("env set string should be `hello`, is ", str)
+	}
+
+}
 
 func TestString(t *testing.T) {
-	s := String("path", "d", "some test env var")
-	t.Logf("%v", s)
-
-	t.Log(environment)
+	ResetForTesting()
+	s := String("conf_string", "foo", "test string")
+	if s != "foo" {
+		t.Errorf("expected: %s got: %s", "foo", s)
+	}
 }
 
 func TestStringOption(t *testing.T) {
-	s := StringOption("test1", "d", []string{"cat", "dog", "d"}, "some test env var")
-	t.Logf("%v", s)
-
-	t.Log(environment)
+	ResetForTesting()
+	s := StringOption("conf_string_opt", "foo", []string{"foo", "bar"}, "set value must be an option")
+	if s != "foo" {
+		t.Errorf("expected: %s got: %s", "foo", s)
+	}
 }
 
-func TestBool(t *testing.T) {
-	s := Bool("something", true, "some test env var")
-	t.Logf("%v", s)
+func TestStringOptionModify(t *testing.T) {
+	ResetForTesting()
+	os.Setenv("CONF_STRING_OPT", "bar")
+	s := StringOption("conf_string_opt", "foo", []string{"foo", "bar"}, "set value must be an option")
+	if s != "bar" {
+		t.Errorf("expected: %s got: %s", "bar", s)
+	}
+}
 
-	t.Log(environment)
+func TestStringOptionInvalid(t *testing.T) {
+	ResetForTesting()
+	os.Setenv("CONF_STRING_OPT", "foobar")
+	s := StringOption("conf_string_opt", "foo", []string{"foo", "bar"}, "set value must be an option")
+	if s != "foo" {
+		t.Errorf("expected: %s got: %s", "bar", s)
+	}
 }
 
 func TestPrintDefaults(t *testing.T) {
+	ResetForTesting()
 	PrintDefaults()
+}
+
+func TestPrintEnv(t *testing.T) {
+	ResetForTesting()
+	PrintEnv()
 }
